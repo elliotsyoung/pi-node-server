6550e028ffd14a107029d467717bea010532291b
Elliot Young <elliotsyoung@gmail.com>
2017-10-15 19:16:35 -0700

    added stop message logic

--- server/config/socket.js
+++ server/config/socket.js
@@ -5,8 +5,12 @@ io.on('connection', function (socket) {
 	console.log(socket.conn.id);
 	socket.on('chat message', function (data) {
 		console.log("MESSAGE:"+ data + " FROM:"["red"] + socket.conn.id);
-		io.emit('chat message', data)
-		const response = new ResponseModel({type: "maya_class", text:data});
+		io.emit('chat message', data) //relay chat message back to all in chat room
+		let readCount = 0
+		if(data === "stop"){
+			readCount=1 //the server will not read the message if the readcount is greater than 0
+		}
+		const response = new ResponseModel({type: "maya_class", text:data, readCount:readCount});
 		response.save().then(err => {console.log(err);}).catch(err => {console.log(err);})
 	});
 	socket.on('subscribe', function (data) {
