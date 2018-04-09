13657ac0c04f030ce12e66ba373b2e69515f3c84
Elliot Young <elliotsyoung@gmail.com>
2018-04-08 19:55:11 -0700

    Finished jukebox server logic

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,9 +7,8 @@ const colors = require('colors');
 
 const multer = require('multer');
 const storage = multer.memoryStorage()
-const upload = multer({ storage });
+const upload = multer({storage});
 const fs = require('fs');
-
 
 // const testPerson = new PersonModel({name: "" + Math.floor(Math.random()*100)}).save().then(err => {console.log(err);});
 
@@ -18,121 +17,122 @@ module.exports = function(app) {
   app.get('/test', (req, res) => {
     console.log("accessed test route.");
     io.to('town hall').emit("chat message", "this is from the server!")
-    res.json({
-      response: "You have accessed the test route."
-    });
+    res.json({response: "You have accessed the test route."});
   });
 
   app.post('/file', upload.single('file'), (req, res) => {
-    fs.writeFile("./server/config/"+req.file.originalname, req.file.buffer, (err) => { if(err) 	console.log(err); })
+    fs.writeFile("./server/config/" + req.file.originalname, req.file.buffer, (err) => {
+      if (err) 
+        console.log(err);
+      }
+    )
     res.json("Success!")
   });
 
   app.get('/', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'index.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'index.html'))
   });
 
   app.get('/david', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'customAlexaResponsePage.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'customAlexaResponsePage.html'))
   });
 
   app.get('/luis', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'customPiResponsePage.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'customPiResponsePage.html'))
     // res.json("Goodbye Kelton.");
   });
 
   app.get('/roman', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'roman.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'roman.html'))
     // res.json("Goodbye Kelton.");
   });
 
   app.get('/nic', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'nic.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'nic.html'))
     // res.json("Goodbye Kelton.");
   });
 
   app.get('/wes', (req, res) => {
     console.log("somebody went to the normal route.");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'wes.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'wes.html'))
     // res.json("Goodbye Kelton.");
   });
 
   app.get('/pete', (req, res) => {
-    res.sendFile(path.join( global.__base, '/client/html' ,'song_adder.html') )
+    res.sendFile(path.join(global.__base, '/client/html', 'song_adder.html'))
   });
 
   app.post('/create_song', (req, res) => {
     const requiredFields = ['song_name', 'artist_first_name', 'artist_last_name', 'genre', 'sound_cloud_link'];
     const missingFields = getMissingFields(requiredFields, req.body)
-    if(missingFields.length < 1){
+    if (missingFields.length < 1) {
       const newSong = new SongModel(req.body);
       newSong.save((err) => {
-        if(err){
+        if (err) {
           console.log(err);
           res.send("Error Saving to Database")
-        } else{
+        } else {
           console.log("======song saved to database:======");
           console.log(req.body);
           console.log("===================================");
           res.redirect("/pete");
         }
       });
-    } else{
+    } else {
       res.send("Missing fields:" + missingFields.join(", "));
     }
   });
 
   app.post('/get_newest_song_from_artist', (req, res) => {
-    const requiredFields = ['artist'];
+    const requiredFields = ['artist_first_name'];
     const missingFields = getMissingFields(requiredFields, req.body);
     if (missingFields.length < 1) {
-      SongModel.find({artist: req.body.artist})
-      .sort({_id:-1})
-      .limit(1)
-      .exec( (err, data) =>{
+      SongModel.find({artist_first_name: req.body.artist_first_name}).sort({_id: -1}).limit(1).exec((err, data) => {
         if (err) {
           console.log(err);
           res.json({response_text: "There was an error with the database."})
         } else {
-          if(data.length > 0){
+          if (data.length > 0) {
             const newestSong = data[0];
-            const response_text = `Here's a new song from ${newestSong.artist} called ${newestSong.name}`;
-            res.json({response_text:response_text});
-          } else{
+            const response_text = `${newestSong.artist_first_name} has a new ${newestSong.genre} song called ${newestSong.song_name}, I'll put it up on the big screen.`;
+            res.json({response_text: response_text});
+            setTimeout(() => {
+              io.to('pi-client').emit('play song', {sound_cloud_link: newestSong.sound_cloud_link});
+            }, 3000);
+          } else {
             res.json({response_text: "I couldn't find any songs from that author, sorry."})
           }
         }
       });
     } else {
-      res.json("error");
+      res.json("ERROR: No Artist Name Given");
     }
   });
 
-  function getMissingFields(requiredFields, submittedInputs){
+  function getMissingFields(requiredFields, submittedInputs) {
     let missingFields = [];
-    for(field of requiredFields){
-      if(!submittedInputs[field]){
+    for (field of requiredFields) {
+      if (!submittedInputs[field]) {
         missingFields.push(field)
       }
     }
     return missingFields;
   }
 
-
   app.get('/clinton', (req, res) => {
-    res.sendFile(path.join( __dirname, '../../client/html' ,'clinton.html'))
+    res.sendFile(path.join(__dirname, '../../client/html', 'clinton.html'))
   });
 
   app.get('/daniel-goodbye-card', (req, res) => {
     console.log("somebody went to the normal route.");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.sendFile(path.join( __dirname, '../../client/html' ,'daniel-goodbye-card.html') )
+    res.sendFile(path.join(__dirname, '../../client/html', 'daniel-goodbye-card.html'))
     // res.json("Goodbye Kelton.");
   });
 
@@ -149,11 +149,7 @@ module.exports = function(app) {
     console.log("socket request recieved");
     console.log(req.body);
     io.to('pi-client').emit(req.body.request_type, req.body);
-    res.json(
-      {
-        response: "You have accessed the test route."
-      }
-    );
+    res.json({response: "You have accessed the test route."});
   });
 
 }; //end of module.exports
--- server/models/song.js
+++ server/models/song.js
@@ -4,18 +4,28 @@ mongoose.Promise = require('bluebird');
 //Define a schema
 const Schema = mongoose.Schema;
 
-const SongSchema = new Schema(
-  {
-    song_name: String,
-    artist_first_name: String,
-    artist_last_name: String,
-    genre: {type: String, default: "no genre"},
-    sound_cloud_url: String,
-    description: {type: String, default: "no description"},
-    play_count: {type: Number, default: 0},
-    date_created: {type: Date, default: Date.now()}
+const SongSchema = new Schema({
+  song_name: String,
+  artist_first_name: String,
+  artist_last_name: String,
+  genre: {
+    type: String,
+    default: "no genre"
+  },
+  sound_cloud_link: String,
+  description: {
+    type: String,
+    default: "no description"
+  },
+  play_count: {
+    type: Number,
+    default: 0
+  },
+  date_created: {
+    type: Date,
+    default: Date.now()
   }
-);
+});
 
 //Export function to create "SomeModel" model class
-module.exports = mongoose.model('song', SongSchema );
+module.exports = mongoose.model('song', SongSchema);
