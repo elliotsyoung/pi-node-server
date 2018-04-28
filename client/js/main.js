e34761509fe83aa89100bb4a2d7a1ccdae94913c
Elliot Young <elliotsyoung@gmail.com>
2018-04-28 00:13:43 -0700

    added more functionality to whiteboard editor page

--- client/js/main.js
+++ client/js/main.js
@@ -6,6 +6,58 @@
   var canvas = document.getElementsByClassName('whiteboard')[0];
   var colors = document.getElementsByClassName('color');
   var context = canvas.getContext('2d');
+  var lineWidth = 2;
+  var eraserWidth = 25;
+  var colorLineWidth = 2;
+  var selectedColor = 'black'
+
+  // EVENT LISTENERS FOR INCREASING AND DECREASING BRUSH SIZE ##########
+  document.getElementById("plus").addEventListener('click', (e) => {
+    lineWidth = colorLineWidth;
+    colorLineWidth += 3;
+    console.log("clicked plus");
+    redrawBrushSize();
+  })
+  document.getElementById("minus").addEventListener('click', (e) => {
+    lineWidth = colorLineWidth;
+    colorLineWidth -= 3;
+    console.log("clicked minus");
+    redrawBrushSize();
+  })
+  document.getElementById("eraser").addEventListener('click', (e) => {
+    lineWidth = eraserWidth;
+    console.log("clicked eraser");
+  })
+
+  function colorButtonClicked(e) {
+    console.log(this.getAttribute("class"));
+
+    if (!this.getAttribute("id")) {
+      lineWidth = colorLineWidth;
+    }
+    redrawBrushSize();
+  }
+
+  var colorButtons = document.getElementsByClassName('color');
+  for (var i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].addEventListener('click', colorButtonClicked, false);
+  }
+  // END OF EVENT LISTENERS FOR INCREASING AND DECREASING BRUSH SIZE ######################
+
+  function redrawBrushSize(ignore) {
+    console.log("worked");
+    context.clearRect(0, 0, canvas.width, 48);
+    context.beginPath();
+    context.moveTo(canvas.width / 2, 24);
+    context.lineTo(canvas.width - 40, 24);
+    context.strokeStyle = selectedColor;
+    context.lineWidth = lineWidth;
+    context.stroke();
+    context.closePath();
+    if (!ignore) {
+      socket.emit('changeBrushSize', lineWidth);
+    }
+  }
 
   var current = {
     color: 'black'
@@ -15,17 +67,22 @@
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+  canvas.addEventListener('mousemove', throttle(onMouseMove, 20), false);
 
   canvas.addEventListener('touchstart', onTouchStart, false);
   canvas.addEventListener('touchend', onTouchEnd, false);
-  canvas.addEventListener('touchmove', throttle(onTouchMove, 10), false);
+  canvas.addEventListener('touchmove', throttle(onTouchMove, 20), false);
 
   for (var i = 0; i < colors.length; i++) {
     colors[i].addEventListener('click', onColorUpdate, false);
   }
 
   socket.on('drawing', onDrawingEvent);
+
+  socket.on('changeBrushSize', (newWidth) => {
+    lineWidth = newWidth;
+    redrawBrushSize(true);
+  })
 
   window.addEventListener('resize', onResize, false);
   onResize();
@@ -35,7 +92,7 @@
     context.moveTo(x0, y0);
     context.lineTo(x1, y1);
     context.strokeStyle = color;
-    context.lineWidth = 2;
+    context.lineWidth = lineWidth;
     context.stroke();
     context.closePath();
 
@@ -109,6 +166,7 @@
 
   function onColorUpdate(e) {
     current.color = e.target.className.split(' ')[1];
+    selectedColor = current.color;
   }
 
   // limit the number of events per second
@@ -125,6 +183,7 @@
   }
 
   function onDrawingEvent(data) {
+    selectedColor = data.color;
     var w = canvas.width;
     var h = canvas.height;
     drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
@@ -135,5 +194,7 @@
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }
+
+  redrawBrushSize();
 
 })();
--- server/config/socket.js
+++ server/config/socket.js
@@ -1,29 +1,34 @@
 var io = require('socket.io')();
 const colors = require('colors');
 const ResponseModel = require('../models/response.js');
-io.on('connection', function (socket) {
-	console.log(socket.conn.id);
-	socket.on('chat message', function (data) {
-		console.log("MESSAGE:"+ data + " FROM:"["red"] + socket.conn.id);
-		io.to('customAlexaResponsePage').emit('chat message', data) //relay chat message back to all in chat room
-		let readCount = 0
-		if(data === "stop"){
-			readCount=1 //the server will not read the message if the readcount is greater than 0
-		}
-		const response = new ResponseModel({type: "maya_class", text:data, readCount:readCount});
-		response.save().then(err => {console.log(err);}).catch(err => {console.log(err);})
-	});
-	socket.on('subscribe', function (data) {
-		console.log("User:"+ socket.conn.id + " IS JOINING ROOM:" + data.room);
-		socket.join(data.room)
-	});
-	socket.on('to room', function(data){
-		console.log(data);
-		if(data.room){
-			io.to(data.room).emit(data.type, data.data)
-		}
-	});
-	socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+io.on('connection', function(socket) {
+  console.log(socket.conn.id);
+  socket.on('chat message', function(data) {
+    console.log("MESSAGE:" + data + " FROM:" ["red"] + socket.conn.id);
+    io.to('customAlexaResponsePage').emit('chat message', data) //relay chat message back to all in chat room
+    let readCount = 0
+    if (data === "stop") {
+      readCount = 1 //the server will not read the message if the readcount is greater than 0
+    }
+    const response = new ResponseModel({type: "maya_class", text: data, readCount: readCount});
+    response.save().then(err => {
+      console.log(err);
+    }).catch(err => {
+      console.log(err);
+    })
+  });
+  socket.on('subscribe', function(data) {
+    console.log("User:" + socket.conn.id + " IS JOINING ROOM:" + data.room);
+    socket.join(data.room)
+  });
+  socket.on('to room', function(data) {
+    console.log(data);
+    if (data.room) {
+      io.to(data.room).emit(data.type, data.data)
+    }
+  });
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('changeBrushSize', (data) => socket.broadcast.emit('changeBrushSize', data));
 });
 
 module.exports = io;
