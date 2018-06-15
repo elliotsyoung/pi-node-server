b441aed1a2c802aa762a61ca4aa2e177e0c160af
Elliot Young <elliotsyoung@gmail.com>
2018-06-14 19:17:06 -0700

    changed chat room to have quick commands feature

--- server/config/socket.js
+++ server/config/socket.js
@@ -32,6 +32,57 @@ io.on('connection', function(socket) {
     socket.emit("message", "Successfully connected to chatroom.")
   });
 
+  // Quick commands is an array of objects, each with a default response and a custom response
+  let QuickCommands = [
+    {
+      defaultText: "Welcome to class!",
+      customText: "Welcome to class 1! I hope you're feeling 2"
+    }, {}, {}
+  ];
+
+  socket.on("pi room chat message", function(msg) {
+    console.log("pi room chat message sent");
+    let text_response;
+    if (isQuickCommand(msg)) {
+      const quickCommandIndex = parseInt(msg.charAt(1)) - 1;
+      const quickCommandArguments = getArgumentsFromMessage(msg);
+      if (quickCommandArguments.length > 0) {
+        text_response = QuickCommands[quickCommandIndex].customText;
+        console.log(text_response);
+        for (var i = 1; i <= quickCommandArguments.length; i++) {
+          const regex = new RegExp(`${i}`, "g");
+          text_response = text_response.replace(regex, quickCommandArguments[i - 1]);
+        }
+      } else {
+        text_response = QuickCommands[quickCommandIndex].defaultText;
+      }
+      io.to("pi-client").emit("pi room chat message", text_response)
+      io.to("pi-client").emit("robot speak command", text_response)
+    } else {
+      io.to("pi-client").emit("pi room chat message", msg)
+      io.to("pi-client").emit("robot speak command", msg)
+    }
+    // HELPER FUNCTIONS ##########
+    function isQuickCommand(message) {
+      // Quick commands need to have a "-" character and they need to exist in the QuickCommands array
+      return message.startsWith("-") && (QuickCommands[message.charAt(1)] != undefined);
+    }
+
+    function getArgumentsFromMessage(message) {
+      // creates an array of strings from the original with the space character being the separator.catch
+      // "-1 Elliot" would become ["-1", "Elliot"]
+      const messageParts = message.split(" ");
+      if (messageParts.length < 2) {
+        // if the message doesn't have at least 2 parts there aren't any arguments available
+        return false;
+      } else {
+        // returns all strings in the message parts except for the first one
+        return messageParts.slice(1);
+      }
+    }
+    // END OF HELPER FUNCTIONS ######################
+  }); // end of pi room chat message
+
   socket.on('to room', function(msg) {
     console.log(msg);
     if (msg.room) {
